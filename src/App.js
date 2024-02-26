4bbe11ca978e9d8ac8d866427d323543dbb45de7
Aman Rawat <rawataman101@gmail.com>
2024-02-26 14:03:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
     <div className="App">
       <h1>Counter App</h1>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 3)}>Increment</button>
-      <button onClick={() => setCount(count - 3)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 }
